9d2b033d9442a248f9820088200d78eaec4671fc
16manishyadav <www.16manishyadav@gmail.com>
2025-07-16 17:30:02 +0530

    Added the firebase credentials as environement variable

--- firebase_utils.py
+++ firebase_utils.py
@@ -1,10 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 from fastapi import HTTPException, Request
+import os
+import json
 
 # Initialize Firebase Admin SDK (only once)
 if not firebase_admin._apps:
-    cred = credentials.Certificate("firebase-service-account.json")
+    if "GOOGLE_APPLICATION_CREDENTIALS_JSON" in os.environ:
+        service_account_info = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
+        cred = credentials.Certificate(service_account_info)
+    else:
+        cred = credentials.Certificate("firebase-service-account.json")
     firebase_admin.initialize_app(cred)
 
 def verify_firebase_token(request: Request):
@@ -12,7 +18,7 @@ def verify_firebase_token(request: Request):
     if not auth_header or not auth_header.startswith("Bearer "):
         raise HTTPException(status_code=401, detail="Missing or invalid Authorization header")
     id_token = auth_header.split("Bearer ")[1]
-    # print(f"Received Firebase ID token: {id_token}")  # Debug print
+    print(f"Received Firebase ID token: {id_token}")  # Debug print
     try:
         decoded_token = auth.verify_id_token(id_token)
         return decoded_token["uid"]
